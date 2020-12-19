679f493d92c87eb2cbb390e4b546244c8539ad57
cliljdn <caliljaudiannn@gmail.com>
2020-12-19 18:14:08 +0800

    fix dev

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const Knex = require('knex')
 const { Model } = require('objection')
 
 // Initialize knex.
-const knex = Knex(knexConfig.client)
+const knex = Knex(knexConfig.development)
 
 // Bind all Models to the knex instance. You only
 // need to do this once before you use any of
