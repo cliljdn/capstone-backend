15810d7adfc8e57be14cefdc322ba6e9c007faf2
cliljdn <caliljaudiannn@gmail.com>
2021-02-28 22:13:09 +0800

    wip -> email

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,29 @@
 require('dotenv').config()
 // Update with your config settings.
 module.exports = {
-	// development: {
-	// 	client: 'mysql',
-	// 	connection: {
-	// 		host: process.env.DB_HOST,
-	// 		database: 'capstonedb',
-	// 		user: 'root',
-	// 		pass: '',
-	// 		// user: 'cliljdn',
-	// 		// password: 'jaudian29',
-	// 		timezone: 'UTC',
-	// 		dateStrings: true,
-	// 	},
-	// 	pool: {
-	// 		min: 2,
-	// 		max: 10,
-	// 	},
-	// 	migrations: {
-	// 		directory: './database/migrations',
-	// 	},
-	// 	seeds: {
-	// 		directory: './database/seeds',
-	// 	},
-	// },
+	development: {
+		client: 'mysql',
+		connection: {
+			host: process.env.DB_HOST,
+			database: 'capstonedb',
+			user: 'root',
+			pass: '',
+			// user: 'cliljdn',
+			// password: 'jaudian29',
+			timezone: 'UTC',
+			dateStrings: true,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: './database/migrations',
+		},
+		seeds: {
+			directory: './database/seeds',
+		},
+	},
 
 	production: {
 		client: 'mysql',
